c7e3622b99292c80ea607b4fb8c6c030ac4ecd7f
LiteX Robot <robot@timvideos.us>
2022-05-26 14:28:02 +0000

    Updating pythondata-cpu-marocchino to 0.0.post201
    Updated data to v0.0-67-g6e010fd based on 6e010fd5b1da54632939052e7007c9c412aae6bb from https://github.com/openrisc/or1k_marocchino.git.
    > commit 6e010fd5b1da54632939052e7007c9c412aae6bb
    > Author: Andrey Bacherov <bandvig@mail.ru>
    > Date:   Wed Feb 9 10:52:12 2022 +0300
    >
    >     Final fix I(D)MMUs super-cache miss detection. Plus some re-factoring.
    >
    
    Updated using 0.0.post134 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_marocchino/__init__.py
+++ pythondata_cpu_marocchino/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/openrisc/or1k_marocchino.git"
 
 # Module version
-version_str = "0.0.post198"
-version_tuple = (0, 0, 198)
+version_str = "0.0.post201"
+version_tuple = (0, 0, 201)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post198")
+    pversion = V("0.0.post201")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Wed Feb 9 10:52:12 2022 +0300
 """
 
 # Tool version info
-tool_version_str = "0.0.post131"
-tool_version_tuple = (0, 0, 131)
+tool_version_str = "0.0.post134"
+tool_version_tuple = (0, 0, 134)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post131")
+    ptool_version = V("0.0.post134")
 except ImportError:
     pass
 
